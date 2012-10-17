d3b8d69ef4e3d1a2f73eb8db793fd97c72e72ed6
matt <matt@56c5sw.com>
2012-10-16 20:52:50 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-QSL-Card-Showcase
+QSL Card Showcase
 =================
 
-Web interface and API to allow you to display, analyze, and retrieve QSL cards
+Tentative name for a project I'm doing in my free time to show off QSL cards received
+by the Rochester Institute of Technology Amateur Radio Club, K2GXT.
+
+In the future I plan on creating a full web interface along with analytic tools and also an API
+to allow people to take their digitized QSL cards and view them easily..
+
+More documentation on how to use this program, as well as updates to it to make it easier for everybody
+to use is in the works.
+
+As of right now this code is provided AS IS, but it will change in the coming weeks.
---
+++ webInterface.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "encoding/json"
+    "io/ioutil"
+    "net/http"
+    "path"
+)
+
+type jsonobject struct {
+    Callsign   string
+    Date   string
+    Front_image   string
+    Back_image   string
+    Mode   string
+    Frequency string
+}
+
+var (
+    jsontype []jsonobject
+    rootdir, _ = os.Getwd()
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<title>K2GXT QSL Cards</title>")
+    fmt.Fprintf(w, "<style> h1 { text-align:center;}</style><h1><img src=\"../images/k2logo.jpg\" width=480 height=120></img></br>QSL Cards</h1></br>")
+    for i := 1; i < len(jsontype); i++ {
+	var call string = jsontype[i].Callsign
+	fmt.Fprintf(w, "<a href=/view/"+call +">" + call + "</a></br>")
+    }
+}
+
+func displayCard(w http.ResponseWriter, r *http.Request) {
+    var callToCheck string = r.URL.Path[6:]
+    fmt.Fprintf(w, "<title>" + callToCheck + "</title>")
+    for i := 1; i < len(jsontype); i++ {
+	if jsontype[i].Callsign == callToCheck {
+	    fmt.Fprintf(w, "<h1>" + jsontype[i].Callsign + "</h1></br>")
+	    fmt.Fprintf(w, "Date: " + jsontype[i].Date)
+	    fmt.Fprintf(w, "</br>Mode: " + jsontype[i].Mode)
+	    fmt.Fprintf(w, "</br>Frequency: " + jsontype[i].Frequency)
+	    var fileName string = "../convertedCards/" + jsontype[i].Front_image
+	    fmt.Fprintf(w, "</br></br>Front:</br><img src=\"" + fileName + "\" width=480 height=320 ></img>")
+	    fmt.Fprintf(w, "</br><a href=../cards/" + jsontype[i].Front_image + "> Download full sized image </a>")
+	    var backName string = "../../convertedCards/" + jsontype[i].Back_image
+	    fmt.Fprintf(w, "</br></br>Back:</br><img src=\"" + backName + "\" width=480 height=320></img>")
+	    fmt.Fprintf(w, "</br><a href=../../cards/" + jsontype[i].Back_image + "> Download full sized image </a>")
+	}
+    }
+}
+
+
+func main() {
+    file, e := ioutil.ReadFile("../QSOs.json")
+    if e != nil {
+        fmt.Printf("File error: %v\n", e)
+        os.Exit(1)
+    }
+
+    json.Unmarshal(file, &jsontype)
+
+    http.Handle("/convertedCards/", http.StripPrefix("/convertedCards",
+        http.FileServer(http.Dir(path.Join(rootdir, "../convertedCards/")))))
+    http.Handle("/cards/", http.StripPrefix("/cards",
+	http.FileServer(http.Dir(path.Join(rootdir, "../cards/")))))
+    http.Handle("/images/", http.StripPrefix("/images",
+	http.FileServer(http.Dir(path.Join(rootdir, "../images/")))))
+
+    http.HandleFunc("/", index)
+    http.HandleFunc("/view/", displayCard)
+    fmt.Printf("http server started\n\n")
+    http.ListenAndServe(":8080", nil)
+}
+
